850f2a9ab67bab006734269e07243bc05a02f1ee
Igor Stevanovic <35437662+Giga99@users.noreply.github.com>
2021-02-28 21:46:37 +0100

    Added DI GetIt

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,16 @@
 import 'package:dartz/dartz.dart' as dz;
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'package:movie_app/data/core/api_client.dart';
-import 'package:movie_app/data/repositories/movie_repository_impl.dart';
 import 'package:movie_app/domain/entities/app_error.dart';
 import 'package:movie_app/domain/entities/movie_entity.dart';
 import 'package:movie_app/domain/entities/no_params.dart';
-import 'package:movie_app/domain/repositories/movie_repository.dart';
 import 'package:movie_app/domain/usecases/get_trending.dart';
-import './data/data_sources/movie_remote_data_source.dart';
+import 'package:pedantic/pedantic.dart';
+import 'di/get_it.dart' as getIt;
 
 void main() async {
-  ApiClient apiClient = ApiClient(Client());
-  MovieRemoteDataSource dataSource = MovieRemoteDataSourceImpl(apiClient);
-  MovieRepository movieRepository = MovieRepositoryImpl(dataSource);
-  GetTrending getTrending = GetTrending(movieRepository);
+  unawaited(getIt.init());
+
+  GetTrending getTrending = getIt.getItInstance<GetTrending>();
   final dz.Either<AppError, List<MovieEntity>> eitherResponse =
       await getTrending(NoParams());
 
