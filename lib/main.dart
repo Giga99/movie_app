e47156ecf20ab7e592ba57738704079100d5c4bb
Igor Stevanovic <35437662+Giga99@users.noreply.github.com>
2021-02-28 21:11:58 +0100

    Added UseCases and MovieRepository with it's implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,34 @@
+import 'package:dartz/dartz.dart' as dz;
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:movie_app/data/core/api_client.dart';
+import 'package:movie_app/data/repositories/movie_repository_impl.dart';
+import 'package:movie_app/domain/entities/app_error.dart';
+import 'package:movie_app/domain/entities/movie_entity.dart';
+import 'package:movie_app/domain/entities/no_params.dart';
+import 'package:movie_app/domain/repositories/movie_repository.dart';
+import 'package:movie_app/domain/usecases/get_trending.dart';
 import './data/data_sources/movie_remote_data_source.dart';
 
-void main() {
+void main() async {
   ApiClient apiClient = ApiClient(Client());
   MovieRemoteDataSource dataSource = MovieRemoteDataSourceImpl(apiClient);
-  dataSource.getTrending();
-  dataSource.getPopular();
-  dataSource.getPlayingNow();
-  dataSource.getComingSoon();
+  MovieRepository movieRepository = MovieRepositoryImpl(dataSource);
+  GetTrending getTrending = GetTrending(movieRepository);
+  final dz.Either<AppError, List<MovieEntity>> eitherResponse =
+      await getTrending(NoParams());
+
+  eitherResponse.fold(
+    (l) {
+      print('error');
+      print(l);
+    },
+    (r) {
+      print('list of movies');
+      print(r);
+    },
+  );
+
   runApp(MyApp());
 }
 
