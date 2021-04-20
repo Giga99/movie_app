bf2bd15b7f9fc8b01351ec050cc86378ae32fb93
Igor Stevanovic <35437662+Giga99@users.noreply.github.com>
2021-04-20 21:42:31 +0200

    Added crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hive/hive.dart';
@@ -16,6 +17,8 @@ void main() async {
   Hive.init(appDocumentDir.path);
   Hive.registerAdapter(MovieTableAdapter());
   unawaited(getIt.init());
+
+  await Firebase.initializeApp();
 
   runApp(MovieApp());
 }
